0b31c8c720a65a71bf31162868df2d1b6b5f39ae
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-19 08:29:46 -0300

    feat: config global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 const app = fastify()
 
 app.register(appRoutes)
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.log(error)
+  } else {
+    // TODO: log to an external tool like DataDog/Sentry/NewRelic
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
 
 export { app }
