b3baa02bc307d7abab2a1b59c379d99d1d9efb0c
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-24 20:16:20 -0300

    feat: add jwt middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,11 @@
+import { makeGetUserProfileUseCase } from '@/use_cases/factories/make_get_user_profile_use_case'
 import { type FastifyRequest, type FastifyReply } from 'fastify'
 
 export async function profileController (request: FastifyRequest, reply: FastifyReply): Promise<FastifyRequest> {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
+  return reply.status(200).send(user)
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { type FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register'
 import { authenticateController } from './controllers/authenticate'
+import { profileController } from './controllers/profile'
+import { verifyJwt } from './middleware/verify_jwt'
 
 export async function appRoutes (app: FastifyInstance): Promise<void> {
   app.post('/users', registerController)
 
   app.post('/sessions', authenticateController)
+
+  app.get('/me', { onRequest: [verifyJwt] }, profileController)
 }
